2d4bdce8e2fb3edef7fbe52c9986f958aadfea5e
Koichi.Shimomura <unieye51@gmail.com>
2021-08-02 07:31:58 +0900

    14. 06 - Creating a MaterialApp and Scaffolding widget - part 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,42 @@
 import 'package:flutter/material.dart';
 import 'home.dart';
 
-void main(){
-  runApp(
-    MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.red
-      ),
+void main() {
+  runApp(MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.red),
       home: Scaffold(
         appBar: AppBar(
           title: Text("App title"),
-          actions: <Widget>[
-            Icon(Icons.search),
-            Icon(Icons.shopping_cart)
-          ],
+          actions: <Widget>[Icon(Icons.search), Icon(Icons.shopping_cart)],
         ),
         drawer: Drawer(),
         body: Home(),
         floatingActionButton: FloatingActionButton(
-          onPressed: (){
-            
-          },
+          onPressed: () {},
           child: Icon(Icons.add),
         ),
-      ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, color: Colors.purple,),
+              // ignore: deprecated_member_use
+              title: Text("Home", 
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.green  
+                ),
+              )
+            ),
+
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person, color: Colors.deepOrange,),
+              // ignore: deprecated_member_use
+              title: Text("Account")
+            ),
+          ],
+        ),
+      )
     )
   );
 }
