7bd10e69b3540a625ddeba6604402549a56a7955
Koichi.Shimomura <unieye51@gmail.com>
2021-08-02 07:46:21 +0900

    16. 08 - Creating a Stateful widget - part 1

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,35 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _HomeState();
+  }
+
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Welcome to my Universe", textDirection: TextDirection.ltr,),
+    return Container(
+      child :Column(
+        children: <Widget>[
+          TextField(),
+          // ignore: deprecated_member_use
+          RaisedButton(
+            onPressed: (){
+
+            },
+            child: Text("Show message"),
+          ),
+          Center(
+            child: Text("Welcome to my Universe", textDirection: TextDirection.ltr,),
+          ),
+      ],
+    ),
     );
   }
 }
